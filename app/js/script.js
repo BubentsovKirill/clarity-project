359049e7d692ff534c9621c1fbc606190feb8806
Kirill Bubentsov <kirill.dm6@gmail.com>
2017-01-30 17:44:45 +0300

    task/[WHD-5] My visitors

--- app/js/script.js
+++ app/js/script.js
@@ -15,7 +15,6 @@ $(document).ready(function () {
                     .animate({opacity: 1, top: '30%'}, 200);
             });
     });
-
     close.click(function () {
         modal
             .animate({opacity: 0, top: '25%'}, 200,
@@ -25,4 +24,17 @@ $(document).ready(function () {
                 }
             );
     });
+
+    $('.nav-visitors').each(function() {
+        $(this).find('li').each(function(i) {
+            $(this).click(function(){
+                $(this).addClass('active').siblings().removeClass('active')
+                    .closest('.visitor-tab').find('.visitor-tab-item').removeClass('active').eq(i).addClass('active');
+            });
+        });
+    });
+
+
 });
+
+
